8fe40d6560dc6ef9eda114b219715d0eb4992f24
Max Weijers <maxweijers@hotmail.nl>
2023-02-04 12:00:31 +0100

    small shoot change

--- GGJ-2023/Assets/_Scripts/PEWPEW.cs
+++ GGJ-2023/Assets/_Scripts/PEWPEW.cs
@@ -118,58 +118,58 @@ public class PEWPEW : MonoBehaviour
 
 
         //CARROT GUN
-        if (bulletCooldown >= bulletreleaselimit * 300) {
-            if (Input.GetMouseButtonDown(0)) {
-                var bullet = Instantiate(Carrot, ProjectileSpawn.position, ProjectileSpawn.rotation);
-                bullet.GetComponent<Rigidbody>().velocity = ProjectileSpawn.forward * bulletSpeed;
-                bulletCooldown = 0;
-            }
-        }
+        //if (bulletCooldown >= bulletreleaselimit * 300) {
+        //    if (Input.GetMouseButtonDown(0)) {
+        //        var bullet = Instantiate(Carrot, ProjectileSpawn.position, ProjectileSpawn.rotation);
+        //        bullet.GetComponent<Rigidbody>().velocity = ProjectileSpawn.forward * bulletSpeed;
+        //        bulletCooldown = 0;
+        //    }
+        //}
 
         //POTATO GUN
-        if (bulletCooldown >= bulletreleaselimit * 500) {
-            if (Input.GetMouseButtonDown(0)) {
-                var bullet = Instantiate(Potato, ProjectileSpawn.position, ProjectileSpawn.rotation);
-                bullet.GetComponent<Rigidbody>().velocity = ProjectileSpawn.forward * bulletSpeed;
-                bulletCooldown = 0;
-            }
-        }
+        //if (bulletCooldown >= bulletreleaselimit * 500) {
+        //    if (Input.GetMouseButtonDown(0)) {
+        //        var bullet = Instantiate(Potato, ProjectileSpawn.position, ProjectileSpawn.rotation);
+        //        bullet.GetComponent<Rigidbody>().velocity = ProjectileSpawn.forward * bulletSpeed;
+        //        bulletCooldown = 0;
+        //    }
+        //}
 
-        //UNION GUN
-        if (bulletCooldown >= bulletreleaselimit * 200) {
-            if (Input.GetMouseButtonDown(0)) {
-                var bullet = Instantiate(Union, ProjectileSpawn.position, ProjectileSpawn.rotation);
-                bullet.GetComponent<Rigidbody>().velocity = ProjectileSpawn.forward * bulletSpeed;
-                bulletCooldown = 0;
-            }
-        }
+        ////UNION GUN
+        //if (bulletCooldown >= bulletreleaselimit * 200) {
+        //    if (Input.GetMouseButtonDown(0)) {
+        //        var bullet = Instantiate(Union, ProjectileSpawn.position, ProjectileSpawn.rotation);
+        //        bullet.GetComponent<Rigidbody>().velocity = ProjectileSpawn.forward * bulletSpeed;
+        //        bulletCooldown = 0;
+        //    }
+        //}
 
-        //BEET GUN
-        if (bulletCooldown >= bulletreleaselimit * 100) {
-            if (Input.GetMouseButtonDown(0)) {
-                var bullet = Instantiate(Beet, ProjectileSpawn.position, ProjectileSpawn.rotation);
-                bullet.GetComponent<Rigidbody>().velocity = ProjectileSpawn.forward * bulletSpeed;
-                bulletCooldown = 0;
-            }
-        }
+        ////BEET GUN
+        //if (bulletCooldown >= bulletreleaselimit * 100) {
+        //    if (Input.GetMouseButtonDown(0)) {
+        //        var bullet = Instantiate(Beet, ProjectileSpawn.position, ProjectileSpawn.rotation);
+        //        bullet.GetComponent<Rigidbody>().velocity = ProjectileSpawn.forward * bulletSpeed;
+        //        bulletCooldown = 0;
+        //    }
+        //}
 
-        //BLUEBERRY GUN
-        if (bulletCooldown >= bulletreleaselimit * 30) {
-            if (Input.GetMouseButtonDown(0)) {
-                var bullet = Instantiate(Blueberry, ProjectileSpawn.position, ProjectileSpawn.rotation);
-                bullet.GetComponent<Rigidbody>().velocity = ProjectileSpawn.forward * bulletSpeed;
-                bulletCooldown = 0;
-            }
-        }
+        ////BLUEBERRY GUN
+        //if (bulletCooldown >= bulletreleaselimit * 30) {
+        //    if (Input.GetMouseButtonDown(0)) {
+        //        var bullet = Instantiate(Blueberry, ProjectileSpawn.position, ProjectileSpawn.rotation);
+        //        bullet.GetComponent<Rigidbody>().velocity = ProjectileSpawn.forward * bulletSpeed;
+        //        bulletCooldown = 0;
+        //    }
+        //}
 
-        //GRAPE GUN
-        if (bulletCooldown >= bulletreleaselimit * 150) {
-            if (Input.GetMouseButtonDown(0)) {
-                var bullet = Instantiate(Grape, ProjectileSpawn.position, ProjectileSpawn.rotation);
-                bullet.GetComponent<Rigidbody>().velocity = ProjectileSpawn.forward * bulletSpeed;
-                bulletCooldown = 0;
-            }
-        }
+        ////GRAPE GUN
+        //if (bulletCooldown >= bulletreleaselimit * 150) {
+        //    if (Input.GetMouseButtonDown(0)) {
+        //        var bullet = Instantiate(Grape, ProjectileSpawn.position, ProjectileSpawn.rotation);
+        //        bullet.GetComponent<Rigidbody>().velocity = ProjectileSpawn.forward * bulletSpeed;
+        //        bulletCooldown = 0;
+        //    }
+        //}
     }
 }
 
