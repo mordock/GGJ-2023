e80adb27cd79a245d10305d05a7908e84e6ba530
Max Weijers <maxweijers@hotmail.nl>
2023-02-05 13:50:31 +0100

    credits

--- GGJ-2023/Assets/WinManager.cs
+++ GGJ-2023/Assets/WinManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinManager : MonoBehaviour
 {
+    public GameObject mounds;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,8 @@ public class WinManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(mounds.transform.childCount <= 0) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
